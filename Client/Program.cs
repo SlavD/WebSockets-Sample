837f84170e49bacc4605439ce7195b8df4c00985
Slawomir Dorzak <slawomir.dorzak@quest.com>
2020-02-15 20:03:34 +0000

    initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Websocket.Client;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitEvent = new ManualResetEvent(false);
+            var url = new Uri("ws://localhost:5000/ws");
+
+            using (var client = new WebsocketClient(url))
+            {
+                client.ReconnectTimeout = TimeSpan.FromSeconds(30);
+                client.ReconnectionHappened.Subscribe(info =>
+                    Console.WriteLine($"Reconnection happened, type: {info.Type}"));
+
+                client.MessageReceived.Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
+                client.Start();
+
+                Task.Run(() => client.Send("test echo 123"));
+
+                exitEvent.WaitOne();
+            }
+
+        }
+    }
+}
